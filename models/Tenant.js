ad4b1fb9d86d5a9a7521b44d1f5addd574289f3f
carlkiptoo <carlosiamcarl@gmail.com>
2025-04-23 00:10:12 +0300

    Tenant schema

---
+++ models/Tenant.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const tenantSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    phone: String,
+    unitNumber: String,
+    assignedBy: {type: mongoose.Schema.Types.ObjectId, ref: "Admin"},
+    createdAt: {type: Date, default: Date.now},
+});
+
+const Tenant = mongoose.model("Tenant", tenantSchema);
+
+export default Tenant;
