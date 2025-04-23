bcefb1aff83ab4f3dac31f1be316f349c30bc90e
carlkiptoo <carlosiamcarl@gmail.com>
2025-04-23 11:46:54 +0300

    token generation and verification

---
+++ utils/utils.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export const generateToken = (userId, role) => {
+    return jwt.sign({userId, role}, JWT_SECRET, {expiresIn: "1d"});
+};
+
+export const verifyToken = (token) => {
+    try {
+        return jwt.verify(token, JWT_SECRET);
+    } catch (error) {
+        return null;
+    }
+};
